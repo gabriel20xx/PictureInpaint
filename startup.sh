e8ce68d982736f39636a399ec365437d495bb77f
gabriel20xx <gabrielfranz31@gmail.com>
2025-03-29 18:09:08 +0100

    impro

--- startup.sh
+++ startup.sh
@@ -4,8 +4,9 @@ git fetch
 git pull origin master
 
 # Install dependencies from requirements.txt
-echo "Installing dependencies from requirements.txt..."
+echo "Installing dependencies"
 pip3 install torch==2.1.0+cu118 xformers==0.0.29.post2 --index-url https://download.pytorch.org/whl/cu118
+echo "Installing dependencies from requirements.txt..."
 pip3 install -r requirements.txt
 
 # Run the Python script nudify.py
