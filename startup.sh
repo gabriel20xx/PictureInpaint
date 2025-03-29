e0a2e20db284c088bd5f416b7cb5be231a2f14d5
gabriel20xx <gabrielfranz31@gmail.com>
2025-03-29 15:50:19 +0100

    improvement

--- startup.sh
+++ startup.sh
@@ -6,7 +6,7 @@ sudo apt update
 
 # Install dependencies from requirements.txt
 echo "Installing dependencies from requirements.txt..."
-pip3 install -r requirements.txt
+pip3 install -r requirements.txt -f https://download.pytorch.org/whl/torch_stable.html
 
 # Run the Python script nudify.py
 echo "Running nudify.py..."
