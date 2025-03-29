c51e06ea19445a5eff40c78b72c5734eeb1f1342
gabriel20xx <gabrielfranz31@gmail.com>
2025-03-29 19:00:48 +0100

    fix

--- startup.sh
+++ startup.sh
@@ -10,7 +10,7 @@ git pull origin master
 
 # Install dependencies from requirements.txt
 echo "Installing dependencies"
-pip3 install torch==2.1.0+cu118 xformers --index-url https://download.pytorch.org/whl/cu118
+pip3 install torch==2.1.0+cu118 xformers==0.0.22.post4 --index-url https://download.pytorch.org/whl/cu118
 echo "Installing dependencies from requirements.txt..."
 pip3 install -r requirements.txt
 
